22dd27c05bbab7fa90d65abb7d2ac80e35b2c3f8
Shavinda Silva <shavinda34@gmail.com>
2024-05-09 08:53:54 +0930

    feat: add required routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Login from "./login.jsx";
+import {useEffect, useState} from "react";
+import Classes from "./Classes.jsx";
+import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
+import Nav from "./Nav.jsx";
+import ClassDetails from "./ClassDetails.jsx";
+import Student from "./Student.jsx";
+import axiosInstance, {setHeaders} from "./Services/ApiService.js";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem('token');
+    if (loggedInUser) {
+      getUser();
+    }
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    setIsLoggedIn(false);
+  };
+
+
+  const setLogin = (userDetails) => {
+    const {data, token} = userDetails;
+    localStorage.setItem('token', token);
+    setIsLoggedIn(true);
+    setUser(data);
+  };
+
+  const getUser = async () => {
+    try {
+      setHeaders();
+      const response = await axiosInstance.get('/user');
+      setIsLoggedIn(true);
+      setUser(response.data.data);
+    } catch (error) {
+      handleLogout();
+      console.error('Login failed:', error);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <Router>
+        {isLoggedIn ? (<Nav user={user} logout={handleLogout}/>) : ''}
+        <Routes>
+          <Route path="/" element={<Login onLogin={setLogin}/>}/>
+          <Route path="/classes" element={<Classes user={user}/>}/>
+          <Route path="/classes/:id" element={<ClassDetails/>}/>
+          <Route path="/student/:id" element={<Student/>}/>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
