464345e8a96d745278a384e9647ff7bebe846681
Shavinda Silva <shavinda34@gmail.com>
2024-05-09 10:34:04 +0930

    style: log out button location

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -51,7 +51,7 @@ const Nav = ({user, logout}) => {
             }
           </div>
         </div>
-        <div>
+        <div className={'flex justify-end'}>
           <button onClick={handleLogout}
                   className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Logout
           </button>
